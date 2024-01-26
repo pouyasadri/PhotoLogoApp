0ee297b1b530f1e3bbe29821fd8661c290d5dc17
pouyasadri <pouyapaghandeh78@gmail.com>
2024-01-26 11:37:21 +0100

    Initial Commit

---
+++ PhotoLogoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoLogoApp
+//
+//  Created by Pouya Sadri on 26/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoLogoApp/PhotoLogoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoLogoAppApp.swift
+//  PhotoLogoApp
+//
+//  Created by Pouya Sadri on 26/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoLogoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
